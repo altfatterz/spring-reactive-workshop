8f938d3a2ed7422f36c179e03637daf987e8bc90
Zoltan Altfatter <altfatterz@gmail.com>
2018-08-31 19:17:34 +0200

    add more examples to the defer part

--- reactor-core-workshop/src/main/java/ch/open/generator/Generator.java
+++ reactor-core-workshop/src/main/java/ch/open/generator/Generator.java
@@ -1,5 +1,8 @@
 package ch.open.generator;
 
+import reactor.core.publisher.Flux;
+
+import java.util.Arrays;
 import java.util.Random;
 
 public class Generator {
@@ -10,6 +13,16 @@ public class Generator {
     public Integer generate() {
         nrOfCalls++;
         return random.nextInt(10);
+    }
+
+    public Iterable<Integer> generateAllBlocking() {
+        nrOfCalls++;
+        return Arrays.asList(1, 2, 3, 4, 5);
+    }
+
+    public Flux<Integer> generateAllReactive() {
+        nrOfCalls++;
+        return Flux.just(1, 2, 3, 4, 5);
     }
 
     public int getNrOfCalls() {
--- reactor-core-workshop/src/test/java/ch/open/Part07DeferTest.java
+++ reactor-core-workshop/src/test/java/ch/open/Part07DeferTest.java
@@ -3,9 +3,13 @@ package ch.open;
 import ch.open.generator.Generator;
 import org.junit.Test;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
+import java.util.Iterator;
+
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.assertThat;
 
 public class Part07DeferTest {
@@ -23,4 +27,43 @@ public class Part07DeferTest {
 
         assertThat(generator.getNrOfCalls(), is(5));
     }
+
+    @Test
+    public void monoToInteger() {
+        Mono<Integer> mono = Mono.just(5);
+        Integer integer = workshop.monoToInteger(mono);
+        assertThat(integer, is(5));
+    }
+
+    @Test
+    public void fluxToIntegers() {
+        Flux<Integer> flux = Flux.range(1, 5);
+        Iterable<Integer> integers = workshop.fluxToIntegers(flux);
+        Iterator<Integer> iterator = integers.iterator();
+        assertThat(iterator.next(), is(1));
+        assertThat(iterator.next(), is(2));
+        assertThat(iterator.next(), is(3));
+        assertThat(iterator.next(), is(4));
+        assertThat(iterator.next(), is(5));
+        assertThat(iterator.hasNext(), is(false));
+    }
+
+    @Test
+    public void slowPublisherFastSubscriber() {
+        Generator generator = new Generator();
+
+        Flux<Integer> flux = workshop.slowPublisherFactSubsciber(generator);
+        assertThat(generator.getNrOfCalls(), is(0));
+
+        StepVerifier.create(flux).expectNextCount(5).verifyComplete();
+    }
+
+    @Test
+    public void fastPublisherSlowSubscriber() {
+        Generator generator = new Generator();
+
+
+    }
+
+
 }
