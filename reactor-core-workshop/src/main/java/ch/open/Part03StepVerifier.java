74d4dbb2951242ce04ccf6397b72a4a97ca06f80
Zoltan Altfatter <altfatterz@gmail.com>
2018-09-06 19:37:44 +0200

    add StepVerifier examples

--- reactor-core-workshop/src/main/java/ch/open/Part03StepVerifier.java
+++ reactor-core-workshop/src/main/java/ch/open/Part03StepVerifier.java
@@ -1,5 +1,15 @@
 package ch.open;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.time.Duration;
+import java.util.function.Supplier;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 /**
  * Learn how to use StepVerifier to test Mono, Flux or any other kind of Reactive Streams Publisher.
  *
@@ -8,7 +18,67 @@ package ch.open;
 
 public class Part03StepVerifier {
 
-//======================================================================================================================
+    public void expectSubscriptionAndThenComplete(Flux<String> flux) {
+        // fail // TO BE ADDED
+
+        StepVerifier.create(flux)
+                .expectSubscription()
+                .expectComplete()
+                .verify(); // TO BE REMOVED
+    }
+
+    public void expectItemsAndThenComplete(Flux<String> flux) {
+        // fail() // TO BE ADDED
+        StepVerifier.create(flux)
+                .expectNext("foo", "bar")
+                .verifyComplete(); // TO BE REMOVED
+    }
+
+    public void expectItemsAndThenError(Flux<String> flux) {
+        // fail() // TO BE ADDED
+        StepVerifier.create(flux)
+                .expectNext("foo", "bar")
+                .expectError()
+                .verify(); // TO BE REMOVED
+    }
+
+    public void expectItemsWithAndThenComplete(Flux<Customer> customers) {
+        // fail() // TO BE ADDED
+        StepVerifier.create(customers)
+                .assertNext(customer -> assertThat(customer.getName()).isEqualTo("John"))
+                .assertNext(customer -> assertThat(customer.getName()).isEqualTo("Jane"))
+                .verifyComplete(); // TO BE REMOVED
+    }
+
+    public void expect5Items(Flux<Long> numbers) {
+        // fail() // TO BE ADDED
+        StepVerifier.create(numbers)
+                .expectNextCount(5)
+                .verifyComplete(); // TO BE REMOVED
+    }
+
+    public void expect3600Items(Supplier<Flux<Long>> supplier) {
+        // fail() // TO BE ADDED
+
+        StepVerifier.withVirtualTime(supplier)
+                .thenAwait(Duration.ofHours(1))
+                .expectNextCount(3600)
+                .verifyComplete(); // TO BE REMOVED
+
+    }
 
 
+
+
+    @Data
+    @AllArgsConstructor
+    static class Customer {
+
+        String name;
+    }
+
+    private void fail() {
+        throw new AssertionError("not implemented");
+    }
 }
+
--- reactor-core-workshop/src/test/java/ch/open/Part03StepVerifierTest.java
+++ reactor-core-workshop/src/test/java/ch/open/Part03StepVerifierTest.java
@@ -1,5 +1,12 @@
 package ch.open;
 
+import ch.open.Part03StepVerifier.Customer;
+import org.junit.Test;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+
 /**
  * Learn how to use StepVerifier to test Mono and Flux publishers.
  *
@@ -9,7 +16,35 @@ public class Part03StepVerifierTest {
 
     Part03StepVerifier workshop = new Part03StepVerifier();
 
-//======================================================================================================================
+    @Test
+    public void expectSubscriptionAndThenComplete() {
+        workshop.expectSubscriptionAndThenComplete(Flux.empty());
+    }
 
+    @Test
+    public void expectItemsAndThenComplete() {
+        workshop.expectItemsAndThenComplete(Flux.just("foo", "bar"));
+    }
+
+    @Test
+    public void expectItemsAndThenError() {
+        workshop.expectItemsAndThenError(Flux.just("foo", "bar").concatWith(Mono.error(new RuntimeException())));
+    }
+
+    @Test
+    public void expectItemsWithAndThenComplete() {
+        workshop.expectItemsWithAndThenComplete(Flux.just(
+                new Customer("John"), new Customer("Jane")));
+    }
+
+    @Test
+    public void expectNextCount() {
+        workshop.expect5Items(Flux.interval(Duration.ofSeconds(1)).take(5));
+    }
+
+    @Test
+    public void virtualTime() {
+        workshop.expect3600Items(() -> Flux.interval(Duration.ofSeconds(1)).take(3600));
+    }
 
 }
