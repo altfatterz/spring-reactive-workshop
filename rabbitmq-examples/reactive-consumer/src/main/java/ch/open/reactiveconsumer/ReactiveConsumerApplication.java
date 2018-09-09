8d8859a0b0b9f2bd300f7b0133aa40055b15a74f
Zoltan Altfatter <altfatterz@gmail.com>
2018-09-09 13:02:56 +0200

    add reactive example

--- rabbitmq-examples/reactive-consumer/src/main/java/ch/open/reactiveconsumer/ReactiveConsumerApplication.java
+++ rabbitmq-examples/reactive-consumer/src/main/java/ch/open/reactiveconsumer/ReactiveConsumerApplication.java
@@ -1,12 +1,40 @@
 package ch.open.reactiveconsumer;
 
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Delivery;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.rabbitmq.QueueSpecification;
+import reactor.rabbitmq.ReactorRabbitMq;
+import reactor.rabbitmq.Receiver;
+import reactor.rabbitmq.Sender;
 
+@Slf4j
 @SpringBootApplication
-public class ReactiveConsumerApplication {
+public class ReactiveConsumerApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(ReactiveConsumerApplication.class, args);
+    }
+
+    private static final String QUEUE = "reactive";
+
+    @Override
+    public void run(String... args) {
+
+        Receiver receiver = ReactorRabbitMq.createReceiver();
+
+        // creating the queue if the producer didn't create it yet, otherwise receiver cannot connect
+        Sender sender = ReactorRabbitMq.createSender();
+        Mono<AMQP.Queue.DeclareOk> queueDeclaration = sender.declareQueue(QueueSpecification.queue(QUEUE));
+
+        Flux<Delivery> messages = receiver.consumeAutoAck(QUEUE);
+
+        queueDeclaration.thenMany(messages)
+                .subscribe(delivery -> log.info("Received message {}", new String(delivery.getBody())));
     }
 }
--- rabbitmq-examples/reactive-producer/src/main/java/ch/open/reactiveproducer/ReactiveProducerApplication.java
+++ rabbitmq-examples/reactive-producer/src/main/java/ch/open/reactiveproducer/ReactiveProducerApplication.java
@@ -1,12 +1,43 @@
 package ch.open.reactiveproducer;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+import reactor.rabbitmq.*;
+
+import java.time.Duration;
 
 @SpringBootApplication
-public class ReactiveProducerApplication {
+@Slf4j
+public class ReactiveProducerApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(ReactiveProducerApplication.class, args);
+    }
+
+    private static final String QUEUE = "reactive";
+
+    @Override
+    public void run(String... args) {
+
+        Sender sender = ReactorRabbitMq.createSender();
+
+        Flux<Long> flux = Flux.interval(Duration.ofSeconds(1));
+
+        Flux<OutboundMessageResult> confirmations = sender.sendWithPublishConfirms(
+                flux.map(i -> new OutboundMessage("", QUEUE, ("Message_" + i).getBytes())));
+
+        sender.declareQueue(QueueSpecification.queue(QUEUE))
+                .thenMany(confirmations)
+                .doOnError(e -> log.error("Send failed", e))
+                .subscribe(result -> {
+                    if (result.isAck()) {
+                        log.info("Message {} sent successfully", new String(result.getOutboundMessage().getBody()));
+                    }
+                });
+
     }
 }
